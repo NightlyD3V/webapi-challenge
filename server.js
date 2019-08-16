904df3f094397d3ef0167aebc697f41eabfb00c3
Tyler Spaulding <TylerSpaulding95@gmail.com>
2019-08-16 19:57:08 -0400

    Initial endpoint set-up for projectRouter and npm scripts for deployment

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const express = require('express');
 const helmet = require('helmet');
 const server = express();
 
+const projectRouter = require('./data/helpers/projectRouter.js');
+
 const port = process.env.PORT || 4000;
 
 server.use(helmet());
 server.use(express.json());
+server.use('/api/projects', projectRouter);
 
 server.get('/', (req, res) => {
     res.send('Don\'t worry be happy!');
